b668289962169eae7fe37c91dad09743c89dad63
Charles <charles.ahmadzadeh@outlook.com>
2016-09-21 14:59:09 +0200

    Update doom-setup.sh

--- doom-setup.sh
+++ doom-setup.sh
@@ -1,5 +1,5 @@
     echo "Installing DOOM..."
-    sudo apt-get install prboom doom-wad-shareware -q4 -y
+    sudo apt-get -y install prboom doom-wad-shareware
     echo "Making sure we are in the right place:"
     cd /home/chip
     wget  -O Doom.png http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-26/256/Doom-1-icon.png
