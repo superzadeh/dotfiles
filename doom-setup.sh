295c1246bb054ed71860ffb6d2d9e6a0615c38f2
Charles <charles.ahmadzadeh@outlook.com>
2016-09-21 14:48:06 +0200

    Create doom-setup.sh

---
+++ doom-setup.sh
@@ -0,0 +1,9 @@
+    echo "Installing DOOM..."
+    sudo apt install prboom doom-wad-shareware -q4 -y
+    echo "Making sure we are in the right place:"
+    cd /home/chip
+    wget  -O Doom.png http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-26/256/Doom-1-icon.png
+    printf 'Here is what you need to add the new Icon within settings:\n
+    \tName: Doom\n
+    \tIcon Path: /home/chip/Doom.png\n
+    \tCommand: prboom\n'
