0a2e6414faba3955cecd906d31e3dd6daa75aa29
Charles <charles.ahmadzadeh@outlook.com>
2016-09-21 14:50:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,38 +5,40 @@ sudo apt-get install ssh zsh nodejs build-essential git
 # Reboot management
 # -------------------------------------
 if [[ -f .doom_install.tmp ]]; then
+# After marshmallow is installed, the chip is rebooted. 
+# This is when Doom is installed.
     cd /home/chip/dotfiles
     ./doom-setup.sh
     rm -f .doom_install.tmp
+else
+    # -------------------------------------
+    # Git Configuration
+    # -------------------------------------
+    git config --global user.email = "superzadeh@outlook.com"
+    git config --global user.name = "superzadeh
+    git config --global push.default simple
+    
+    # -------------------------------------
+    # Bootstrap the dotfiles
+    # -------------------------------------
+    # Get the dotfiles
+    git clone http://github/superzadeh/dotfiles
+    cd /home/chip/dotfiles
+    ./bootstrap.sh
+    # Get Vundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    # Start VIM and install plugins
+    vim +PluginInstall +qall
+    
+    # -------------------------------------
+    # Install Mednafen
+    # -------------------------------------
+    cd /home/chip/dotfiles
+    ./madfen.sh
+    
+    # -------------------------------------
+    # Start Marshmallow's Pocket Home installation, thanks to https://gist.github.com/kevinkn/52a8fb5d670bfa40c766e5fb3c2a0696
+    # -------------------------------------
+    cd /home/chip/dotfiles
+    ./marsh-setup.sh
 fi
-
-# -------------------------------------
-# Git Configuration
-# -------------------------------------
-git config --global user.email = "superzadeh@outlook.com"
-git config --global user.name = "superzadeh
-git config --global push.default simple
-
-# -------------------------------------
-# Bootstrap the dotfiles
-# -------------------------------------
-# Get the dotfiles
-git clone http://github/superzadeh/dotfiles
-cd /home/chip/dotfiles
-./bootstrap.sh
-# Get Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Start VIM and install plugins
-vim +PluginInstall +qall
-
-# -------------------------------------
-# Install Mednafen
-# -------------------------------------
-cd /home/chip/dotfiles
-./madfen.sh
-
-# -------------------------------------
-# Start Marshmallow's Pocket Home installation, thanks to https://gist.github.com/kevinkn/52a8fb5d670bfa40c766e5fb3c2a0696
-# -------------------------------------
-cd /home/chip/dotfiles
-./marsh-setup.sh
