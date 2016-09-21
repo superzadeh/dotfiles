ceb96ba872ea170595d7ab64808cc5fa0aaa748d
Charles <charles.ahmadzadeh@outlook.com>
2016-09-21 14:59:44 +0200

    Update marsh-setup.sh

--- marsh-setup.sh
+++ marsh-setup.sh
@@ -2,8 +2,6 @@ mkdir ~/.pocket-home/
 for i in ~/.pocket-home/version ~/.pocket-home/package.txt ~/.pocket-home/link ~/.pocket-home/.version
     do touch $i && chmod +w $i
 done
-echo 'Updating apt...'
-sudo apt update
 echo 'Downloading new package link'
 wget  -O ~/.pocket-home/package.txt 'https://drive.google.com/uc?export=download&id=0B1jRc4IqT9kiSVVIOE8wd2w4Zkk' &&\
 echo 'Checking if new version is available'
@@ -17,6 +15,4 @@ cat ~/.pocket-home/package.txt | tail -1 > ~/.pocket-home/link
     wget  -O ~/.pocket-home/package.deb -i ~/.pocket-home/link && \
     sudo dpkg -i ~/.pocket-home/package.deb && \
     echo 'Installation finished.')
-echo "Rebooting now. You will need to rerun the script to finish the Installation."
-touch .doom_install.tmp
-reboot
+
