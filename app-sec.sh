b191fdcee316b4abf40de031f963d08bd096c089
Charles <charles.ahmadzadeh@outlook.com>
2016-09-26 14:11:16 +0200

    added sslstrip

--- app-sec.sh
+++ app-sec.sh
@@ -2,4 +2,11 @@
 sudo echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # Install tools
-sudo apt-get install tcpdump nmap
+sudo apt-get install tcpdump nmap  python-twisted-web
+
+# Install SSL Strip
+cd $HOME
+mkdir -p app-sec
+cd app-sec
+git clone https://github.com/moxie0/sslstrip
+python setup.py install
