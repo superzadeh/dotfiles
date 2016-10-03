816432d6d8a64c3be5c73d763dd12b42a73d1c59
Charles <charles.ahmadzadeh@outlook.com>
2016-10-03 15:08:52 +0200

    Create vim-setup.sh

---
+++ vim-setup.sh
@@ -0,0 +1,30 @@
+# Install prerequisites
+sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev git
+
+# Remove previous VIM installations
+sudo apt-get remove vim vim-runtime gvim
+
+# Clone and build vim
+cd ~
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-python3interp \
+            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --enable-perlinterp \
+            --enable-luainterp \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
+sudo make install
+
+# Allow to easily uninstall the package using checkinstall
+sudo apt-get install checkinstall
+cd vim
+sudo checkinstall
