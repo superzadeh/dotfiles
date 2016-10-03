19933015c5ee0417990c9ec7972695ccb2a54d37
Charles <charles.ahmadzadeh@outlook.com>
2016-10-03 15:14:07 +0200

    Update vim-setup.sh

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 # Install prerequisites
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev git
