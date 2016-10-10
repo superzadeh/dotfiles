8158d5bfa349c8c091be17b09b2d0ad668ac4817
Charles <charles.ahmadzadeh@outlook.com>
2016-10-10 11:46:55 +0200

    Update go-setup.sh

--- go-setup.sh
+++ go-setup.sh
@@ -1,10 +1,11 @@
+cd $HOME
+
 # Download go
-cd /usr/local/
-git clone https://go.googlesource.com/go
-cd go
-git checkout go1.7.1
-cd src
-./all.bash
+sudo curl -O https://storage.googleapis.com/golang/go1.7.1.linux-armv6l.tar.gz
+# Unzip and Install Go
+sudo tar -xvf go1.7.1.linux-armv6l.tar.gz
+sudo rm -rf /usr/local/go
+sudo mv go /usr/local
 
 # Set Go Path
 sudo echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
@@ -13,3 +14,6 @@ sudo echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.profile
 mkdir -p Go
 sudo echo 'export GOPATH=$HOME/Go' >> ~/.profile
 source ~/.profile
+
+# Cleanup
+rm go1.7.1.linux-armv6l.tar.gz
