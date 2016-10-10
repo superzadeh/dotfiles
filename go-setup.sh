46b559150eac720ef7cda44eed4ae462060cd2e8
Charles <charles.ahmadzadeh@outlook.com>
2016-10-10 10:57:19 +0200

    added cleanup, fixed default workspace

--- go-setup.sh
+++ go-setup.sh
@@ -10,6 +10,9 @@ sudo mv go /usr/local
 sudo echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 sudo echo 'export GOROOT=$HOME/go' >> ~/.profile
 sudo echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.profile
-mkdir Go
-sudo echo 'export GOPATH=$HOME/work' >> ~/.profile
+mkdir -p Go
+sudo echo 'export GOPATH=$HOME/Go' >> ~/.profile
 source ~/.profile
+
+# Cleanup
+rm go1.7.1.linux-amd64.tar.gz
