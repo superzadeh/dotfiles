d7525910528bb3fea034f64fa8b708dc4758d00c
Charles <charles.ahmadzadeh@outlook.com>
2017-02-17 17:19:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,18 +13,6 @@ git config --global user.email = "superzadeh@outlook.com"
 git config --global user.name = "superzadeh"
 git config --global push.default simple
 
-# -------------------------------------
-# Bootstrap the dotfiles
-# -------------------------------------
-# Get the dotfiles
-git clone http://github/superzadeh/dotfiles
-cd $HOME/dotfiles
-chmod +x ./bootstrap.sh
-./bootstrap.sh
-# Get Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Start VIM and install plugins
-vim +PluginInstall +qall
 # Fix keymapping for "minus" and "plus"
 sed -i 's/keycode 82 = KP_Subtract underscore F11 KP_Subtract/keycode 82 = minus underscore F11 minus/g'  $HOME/.Xmodmap
 sed -i 's/keycode 86 = KP_Add KP_Equal F12 KP_Add/keycode 86 = plus equal F12 plus/g' $HOME/.Xmodmap
