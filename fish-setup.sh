9ceb162c2f521e158fc94ca57bf63470996db8ea
Charles <charles.ahmadzadeh@outlook.com>
2017-02-20 12:04:13 +0100

    Update fish-setup.sh

--- fish-setup.sh
+++ fish-setup.sh
@@ -1,3 +1,4 @@
 sudo apt-get install fish python3-dev python3-pip -y
 sudo -H pip3 install thefuck
 echo 'thefuck --alias | source' > ~/.config/fish/config.fish
+echo 'set fish_greeting ""' >> ~/.config/fish/config.fish
