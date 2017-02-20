ae54b519196e7bae94729c1d51eef0f0c501f404
Charles <charles.ahmadzadeh@outlook.com>
2017-02-20 12:02:22 +0100

    Update fish-setup.sh

--- fish-setup.sh
+++ fish-setup.sh
@@ -1,8 +1,3 @@
 sudo apt-get install fish python3-dev python3-pip -y
-
-curl -L http://get.oh-my.fish | fish
-
 sudo -H pip3 install thefuck
-
-omf install chain
-omf install thefuck
+echo 'thefuck --alias | source' > ~/.config/fish/config.fish
