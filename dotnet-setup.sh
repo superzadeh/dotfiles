297f99616f127c3ca7a2ccb17546c2f3a917d433
= <=>
2017-02-27 14:32:47 +0000

    make dotnet script executable

--- dotnet-setup.sh
+++ dotnet-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install curl libunwind8 gettext
+sudo apt-get install curl libunwind8 gettext -y
 curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=835021
 sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
 sudo ln -s /opt/dotnet/dotnet /usr/local/bin
