21772082d954f28f0d5e9eaf8a9fc5307ef19261
Charles <charles.ahmadzadeh@outlook.com>
2017-04-03 10:47:19 +0200

    Create dosbox-setup.sh

---
+++ dosbox-setup.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+sudo apt-get install -y dosbox
+
+mkdir -p ~/dos/c
+
+# Optimize dosbox performance
+mv ~/.dosbox/dosbox-0.74.conf ~/.dosbox/dosbox-0.74.conf.backup
+wget -O ~/.dosbox/dosbox-0.74.conf http://bit.ly/29Qra5D
+
+cd ~/dos/c
+
+wget http://www.bestoldgames.net/download/bgames/prince-of-persia.zip
+unzip prince-of-persia.zip
+
+wget http://www.bestoldgames.net/download/bgames/wolfenstein-3d.zip
+unzip wolfenstein-3d.zip
+
+wget http://www.bestoldgames.net/download/bgames/golden-axe.zip
+unzip golden-axe.zip
+
+cd
